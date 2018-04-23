23b3be487c46999b4bea4248a7bbfa0544c27a42
Trapadore <vaz.vicente.mc@gmail.com>
2018-04-23 17:24:05 +0200

    Ajout du contenu de la classe DogfightModel et de son interface.

--- src/jpu2018/dogfight/model/DogfightModel.java
+++ src/jpu2018/dogfight/model/DogfightModel.java
@@ -2,5 +2,28 @@ package jpu2018.dogfight.model;
 import java.util.Observable;
 
 public class DogfightModel implements IDogfightModel{
-	
+	public DogfightModel() {
+		
+	}
+	public IArea getArea() {
+		
+	}
+	public void buildArea(DImension dimension) {
+		
+	}
+	public void addMobile(IMobile Mobile) {
+		
+	}
+	public void removeMobile(IMobile Mobile) {
+		
+	}
+	public IMobile getMobile(){
+		
+	}
+	public IMobile getMobileByPlayer(int player) {
+		
+	}
+	public void setMobileHavesMoved() {
+		
+	}
 }
--- src/jpu2018/dogfight/model/IDogfightModel.java
+++ src/jpu2018/dogfight/model/IDogfightModel.java
@@ -1,5 +1,11 @@
 package jpu2018.dogfight.model;
 
 public interface IDogfightModel {
-
+	public IArea getArea();
+	public void buildArea(Dimension dimension);
+	public void addMobile(IMobile Mobile);
+	public void removeMobile(IMobile Mobile);
+	public IMobile getMobile();
+	public IMobile getMobileByPlayer(int player);
+	public void setMobileHavesMoved();
 }
