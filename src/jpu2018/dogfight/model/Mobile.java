ce7a0f7cafc6d34203618c1cd0400edca43fb705
1699267 <1699267@10.145.128.70>
2018-04-23 15:03:05 +0200

    mobile model implementation methods

--- src/jpu2018/dogfight/model/Mobile.java
+++ src/jpu2018/dogfight/model/Mobile.java
@@ -1,6 +1,112 @@
 package jpu2018.dogfight.model;
 import java.awt.Color;
+import java.awt.Image;
+import java.awt.Point;
 
 public class Mobile {
-
+	public Mobile(Direction direction, Position position, Dimension dimension, int speed, String image) {
+		
+	}
+	
+	public Direction getDirection() {
+		return null;
+	}
+	
+	public void setDirection(Direction direction) {
+		
+	}
+	
+	public Point getposition() {
+		return null;
+		
+	}
+	
+	public Dimension getDimension() {
+		return null;
+		
+	}
+	
+	public int getSpeed() {
+		return 0;
+		
+	}
+	
+	public int getWidth() {
+		return 0;
+		
+	}
+	
+	public int getHeight() {
+		return 0;
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void placeInArea(IArea area) {
+		
+	}
+	
+	public boolean isPlayer(int player) {
+		return false;
+		
+	}
+	
+	private void mooveUp() {
+		
+	}
+	
+	private void mooveRight() {
+		
+	}
+	
+	private void mooveDown() {
+		
+	}
+	
+	private void mooveLeft() {
+		
+	}
+	
+	public Color getColor() {
+		return null;
+		
+	}
+	
+	public IDogfightModel getDogfightMode() {
+		return null;
+		
+	}
+	
+	public void setDogfightMode(IDogfightModel dogfightModel) {
+		
+	}
+	
+	public boolean hit() {
+		return false;
+		
+	}
+	
+	public boolean isWeapon() {
+		return false;
+		
+	}
+	
+	public Image getImage() {
+		return null;
+		
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
+
